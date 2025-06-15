cde45f6d9e0986597cf4f61e990971814e921c94
Gaya34 <84956226+Gaya34@users.noreply.github.com>
2025-06-15 16:54:06 +0530

    Create __init__.py

---
+++ resources/pom/homepage/__init__.py
@@ -0,0 +1,7 @@
+from PlaywrightCluster.keywords_generator import KeywordsGenerator
+
+
+# pylint: disable=C0103
+class page(KeywordsGenerator):
+    def __init__(self):
+        super().__init__(__file__)
